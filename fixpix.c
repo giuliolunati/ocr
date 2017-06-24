ccb073487c3861838b0c2ddf577baece6c40adc9
Giulio Lunati <giuliolunati@gmail.com>
2017-06-24 14:26:55 +0200

    Add contrast_image(), normalize_image()

--- fixpix.c
+++ fixpix.c
@@ -25,10 +25,13 @@ if (! topic) {
   FILENAME:       load a PNM image\n\
   -:              load from STDIN\n\
   bg FLOAT:       find background light\n\
+  contrast BLACK WHITE:\n\
+                  enhance contrast\n\
   div:            divide im2 / im1\n\
   fix-bg:         fix background light\n\
   histo:          histogram\n\
   lpp FLOAT:      set lines-per-page\n\
+  norm STRENGTH:  normalize contrast\n\
   quit:           quit w/out output\n\
   rot ANGLE:      rotate\n\
   splitx X:       split vertically\n\
@@ -40,6 +43,8 @@ if (! topic) {
   \n");
 } else if (TOPIC("bg")) {;
   printf("Coming soon...\n");
+} else if (TOPIC("contrast")) {;
+  printf("Coming soon...\n");
 } else if (TOPIC("div")) {;
   printf("Coming soon...\n");
 } else if (TOPIC("fix-bg")) {;
@@ -47,6 +52,8 @@ if (! topic) {
 } else if (TOPIC("histo")) {;
   printf("Coming soon...\n");
 } else if (TOPIC("lpp")) {;
+  printf("Coming soon...\n");
+} else if (TOPIC("norm")) {;
   printf("Coming soon...\n");
 } else if (TOPIC("quit")) {;
   printf("Coming soon...\n");
@@ -132,6 +139,15 @@ void destroy_vector(vector *h) {
   if (! h) return;
   if (h->data) free(h->data);
   free(h);
+}
+
+vector *copy_vector(vector *v0) {
+  vector *v = make_vector(v0->size);
+  v->len = v0->len;
+  v->x0 = v0->x0;
+  v->dx = v0->dx;
+  memcpy(v->data, v0->data, v->len * sizeof(*(v->data)));
+  return v;
 }
 
 void cumul_vector(vector *v) {
@@ -480,6 +496,125 @@ vector *threshold_histogram(image *im) {
   return hi;
 }
 
+void contrast_image(image *im, real black, real white) {
+  vector *v = make_vector(256);
+  v->len = v->size;
+  real m = 255.0 * K / (white - black);
+  real q = K_2 - black * m;
+  real t;
+  uint i;
+  for (i = 0; i < v->len; i++) {
+    t = m * i + q;
+    if (t < 0) t = 0;
+    else
+    if (t > MAXVAL) t = MAXVAL;
+    v->data[i] = t;
+  }
+  ulong l = im->width * im->height;
+  short *p;
+  for (p = im->data; p < im->data + l; p++) {
+    *p = v->data[*p / K];
+  }
+  destroy_vector(v);
+}
+
+void normalize_image(image *im, real strength) {
+  uint i, j;
+  vector *v0 = histogram_of_image(im);
+  vector *v1 = copy_vector(v0);
+  real w, b, max0, max1, *p = v1->data;
+  uint c, c1;
+  for (i = 0; i < v1->len; i ++, p ++) *p *= i;
+  cumul_vector(v0);
+  cumul_vector(v1);
+  max0 = v0->data[255];
+  max1 = v1->data[255];
+  c = max1 / max0;
+  for (i = 0; i < 100; i ++) {
+    b = v1->data[c] / v0->data[c];
+    w = (max1 - v1->data[c]) / (max0 -v0->data[c]);
+    c1 = (b + w + 1) / 2;
+    if (c1 == c) break;
+    c = c1;
+  }
+  //printf("%f %f\n", b, w);
+
+  uint x1, x2;
+  real y1, y2, m, q, mb, qb, mw, qw, yb, yw;
+  for (j = 0; j < 9; j ++) {
+    // gray zone
+    x1 = (3*b + w) / 4;
+    x2 = (b + 3*w) / 4;
+    y1 = v0->data[x1];
+    y2 = v0->data[x2];
+    m = (y2 - y1) / (x2 - x1);
+    q = (y1 - m * x1) ;
+
+    // black zone
+    yb = m * b + q;
+    y1 = yb * 0.25;
+    y2 = yb * 0.75;
+    for (i = 0; i < 256; i ++) {
+      x1 = i;
+      if (v0->data[i] <= y1) continue;
+      break;
+    }
+    for (i = x1; i < 256; i ++) {
+      x2 = i;
+      if (v0->data[i] < y2) continue;
+      break;
+    }
+    y1 = v0->data[x1];
+    y2 = v0->data[x2];
+    mb = (y2 - y1) / (x2 - x1);
+    qb = (y1 - mb * x1);
+
+    // white zone
+    yw = m * w + q;
+    y1 = (3*yw + max0) / 4;
+    y2 = (yw + 3*max0) / 4;
+    for (i = 255; i >= 0 ; i --) {
+      x2 = i;
+      if (v0->data[i] >= y2) continue;
+      break;
+    }
+    for (i = x2; i >=0; i --) {
+      x1 = i;
+      if (v0->data[i] >= y1) continue;
+      break;
+    }
+    y1 = v0->data[x1];
+    y2 = v0->data[x2];
+    mw = (y2 - y1) / (x2 - x1);
+    qw = (y2 - mw * x2) ;
+
+    y1 = (q - qb) / (mb - m);
+    y2 = (q - qw) / (mw - m);
+
+    if (hypot(y1-b, y2-w) < 0.1) break;
+    b = y1; w = y2;
+
+    //printf("b: %f w: %f \n", b, w);
+  }
+
+  // black zone: [x1..b]
+  // white zone: [w..x2]
+  x1 = - qb / mb;
+  x2 = (max0 - qw) / mw;
+  c = (b + w + x1 + x2) / 4;
+  // choose black and white
+  b += (x1 - b) * (1 - strength);
+  w += (x2 - w) * (1 - strength);
+  if (w <= b) {
+    b = c;
+    w = b + 1;
+  }
+
+  contrast_image(im, b, w);
+
+  destroy_vector(v0);
+  destroy_vector(v1);
+}
 
 //// STACK ////
 #define STACK_SIZE 256
@@ -529,6 +664,11 @@ int main(int argc, char **args) {
       push(image_background((image*)SP_1));
     }
     else
+    if (ARG_IS("contrast")) { // norm BLACK WHITE
+      if (! *(++arg)) error("norm: missing parameter");
+      if (! *(++arg)) error("norm: missing parameter");
+      contrast_image((image*)SP_1, atof(*(arg-1)), atof(*arg));
+    }
     if (ARG_IS("div")) { // div
       divide_image((image*)SP_2, (image*)SP_1);
       pop();
@@ -546,21 +686,26 @@ int main(int argc, char **args) {
     }
     else
     if (ARG_IS("lpp")) { // lpp FLOAT
-      if (! *(++arg)) break;
+      if (! *(++arg)) error("lpp: missing parameter");
       default_lpp = atof(*arg);
       if (sp) ((image*)SP_1)->lpp = default_lpp;
+    }
+    else
+    if (ARG_IS("norm")) { // norm FLOAT
+      if (! *(++arg)) error("norm: missing parameter");
+      normalize_image((image*)SP_1, atof(*arg));
     }
     else
     if (ARG_IS("quit")) exit(0); // quit
     else
     if (ARG_IS("rot")) { // rot +-90, 180, 270
-      if (! *(++arg)) break;
+      if (! *(++arg)) error("rot: missing parameter");
       push(rotate_image((image*)SP_1, atof(*arg)));
       swap(); pop();
     }
     else
     if (ARG_IS("splitx")) { // splitx FLOAT
-      if (! *(++arg)) break;
+      if (! *(++arg)) error("splitx: missing parameter");
       push(0); swap();
       push(0); swap();
       splitx_image(&SP_2, &SP_3, (image*)SP_1, atof(*arg));
@@ -568,7 +713,7 @@ int main(int argc, char **args) {
     }
     else
     if (ARG_IS("splity")) { // splity FLOAT
-      if (! *(++arg)) break;
+      if (! *(++arg)) error("splity: missing parameter");
       push(0); swap();
       push(0); swap();
       splity_image(&SP_2, &SP_3, (image*)SP_1, atof(*arg));
@@ -582,8 +727,7 @@ int main(int argc, char **args) {
     }
     else
     if (ARG_IS("w")) { // w FILENAME
-      if (! *(++arg)) break;
-      if (sp <= 0) error("Empty stack.");
+      if (! *(++arg)) error("w: missing parameter");
       p = pop();
       f = fopen(*arg, "wb");
       if (IS_IMAGE(p)) write_image(p, f);
